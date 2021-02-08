fb94a26de0930171204327a7a0445b693e8ebbbc
ollienorman <oliver.norman1991@gmail.com>
2021-02-08 18:54:07 +0000

    reformatted feature test for readability

--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -8,6 +8,8 @@ describe 'scenario' do
     account.deposit('10/01/2012', 1000)
     account.deposit('13/01/2012', 2000)
     account.withdraw('14/01/2012', 500)
-    expect { account.statement.print_statement }.to output("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00 \n13/01/2012 || 2000.00 || || 3000.00 \n10/01/2012 || 1000.00 || || 1000.00 \n").to_stdout
+    expect { account.statement.print_statement }
+      .to output("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00 \n13/01/2012 || 2000.00 || || 3000.00 \n10/01/2012 || 1000.00 || || 1000.00 \n")
+      .to_stdout
   end
 end
