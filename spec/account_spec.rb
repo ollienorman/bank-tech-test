e62e0b88194f00b84ac4c9cae09dfadace1eb3bb
ollienorman <oliver.norman1991@gmail.com>
2021-02-08 11:36:13 +0000

    first test for withdraw written - added second expect to deposit test for stronger test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -4,6 +4,15 @@ describe Account do
   describe '#deposit' do
     it 'allows money to be added to the account' do
       expect { subject.deposit(1000) }.to change { subject.balance }.by(1000)
+      expect(subject.balance).to eq 1000
+    end
+  end
+
+  describe '#withdraw' do
+    it 'allows money to be withdrawn from the account' do
+      subject.deposit(1000)
+      expect { subject.withdraw(1000) }.to change { subject.balance }.by(1000)
+      expect(subject.balance).to eq 0
     end
   end
 
