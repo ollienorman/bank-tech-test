9ef25f248de06aa27aa8d795777bd5ead298c12c
ollienorman <oliver.norman1991@gmail.com>
2021-02-08 15:37:51 +0000

    update transaction class to take keyword arguments that default to nil

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -2,11 +2,11 @@ class Transaction
 
   attr_reader :date, :credit, :debit, :balance
 
-  def initialize
-    @date = nil
-    @credit = nil
-    @debit = nil
-    @balance = nil
+  def initialize(date: nil, credit: nil, debit: nil, balance: nil)
+    @date = date
+    @credit = credit
+    @debit = debit
+    @balance = balance
   end
 
 end
