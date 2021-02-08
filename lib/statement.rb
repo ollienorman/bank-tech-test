61e4af94ab7126061aa5dc0a8f6aecec8e7be6bd
ollienorman <oliver.norman1991@gmail.com>
2021-02-08 15:32:53 +0000

    first test for statement class passed - it can create a transaction via dependency injection

--- lib/statement.rb
+++ lib/statement.rb
@@ -1,3 +1,13 @@
+require 'transaction'
+
 class Statement
+
+  def initialize(transaction = Transaction)
+    @transaction = transaction
+  end
+
+  def new_transaction
+    @transaction.new
+  end
 
 end
--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,9 +1,14 @@
 require 'statement'
 
 describe Statement do
+
+  let(:transaction) { double :transaction }
+  let(:transaction_class) { double :transaction_class, new: transaction }
+  subject { described_class.new(transaction_class) }
+
   describe '#new_transaction' do
     it 'can create a new transaction object' do
-      expect(subject.new_transaction).to be_a(Transaction)
+      expect(subject.new_transaction).to eq transaction
     end
   end
 end
