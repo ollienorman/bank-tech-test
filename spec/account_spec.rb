d5138a72090b30411b1e93fe24e0fa7a81e9d995
ollienorman <oliver.norman1991@gmail.com>
2021-02-09 15:32:14 +0000

    updated tests to revert back to automated date fetching, rather than user input

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -3,6 +3,7 @@
 require 'account'
 
 describe Account do
+  time = Time.new.strftime("%d/%m/%Y")
   let(:transaction_history) {double :transaction_history, new_transaction: true}
   let(:statement) { double :statement, transaction_history: transaction_history }
   let(:statement_class) { double :statement_class, new: statement }
@@ -10,24 +11,24 @@ describe Account do
 
   describe '#deposit' do
     it 'allows money to be added to the account' do
-      expect { subject.deposit('01/01/2000', 1000) }.to change { subject.balance }.by(1000)
+      expect { subject.deposit(1000) }.to change { subject.balance }.by(1000)
     end
 
     it 'adds a new transaction to the accounts statement' do
-      expect(subject.statement.transaction_history).to receive(:new_transaction).with(date: '01/01/2000', credit: 1000, balance: 1000)
-      subject.deposit('01/01/2000', 1000)
+      expect(subject.statement.transaction_history).to receive(:new_transaction).with(date: time, credit: 1000, balance: 1000)
+      subject.deposit(1000)
     end
   end
 
   describe '#withdraw' do
     it 'allows money to be withdrawn from the account' do
-      subject.deposit('01/01/2000', 1000)
-      expect { subject.withdraw('01/01/2000', 1000) }.to change { subject.balance }.by(-1000)
+      subject.deposit(1000)
+      expect { subject.withdraw(1000) }.to change { subject.balance }.by(-1000)
     end
 
     it 'adds a new transaction to the accounts statement' do
-      expect(subject.statement.transaction_history).to receive(:new_transaction).with(date: '01/01/2000', debit: 1000, balance: -1000)
-      subject.withdraw('01/01/2000', 1000)
+      expect(subject.statement.transaction_history).to receive(:new_transaction).with(date: time, debit: 1000, balance: -1000)
+      subject.withdraw(1000)
     end
   end
 
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -5,9 +5,12 @@ require 'account'
 describe 'scenario' do
   it 'prints correct statement after three separate bank transactions' do
     account = Account.new
-    account.deposit('10/01/2012', 1000)
-    account.deposit('13/01/2012', 2000)
-    account.withdraw('14/01/2012', 500)
+    allow(account).to receive(:timestamp) { '10/01/2012' }
+    account.deposit(1000)
+    allow(account).to receive(:timestamp) { '13/01/2012' }
+    account.deposit(2000)
+    allow(account).to receive(:timestamp) { '14/01/2012' }
+    account.withdraw(500)
     expect { account.statement.print_statement }
       .to output("date || credit || debit || balance\n14/01/2012 || || 500.00 || 2500.00 \n13/01/2012 || 2000.00 || || 3000.00 \n10/01/2012 || 1000.00 || || 1000.00 \n")
       .to_stdout
